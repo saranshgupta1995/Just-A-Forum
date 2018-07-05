35df7dcbc0b382f8ba8680943a0447c011183ebc
Saransh Gupta <saranshgupta1995@gmail.com>
2018-07-05 22:58:12 +0530

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', component: AppComponent },
+]
 
 @NgModule({
     declarations: [
         AppComponent,
-        LoginComponent
+        LoginComponent,
+        HomeComponent
     ],
     imports: [
+        RouterModule.forRoot(appRoutes,{enableTracing:true}),
         BrowserModule,
         FormsModule
     ],
