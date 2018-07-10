948eca1f1b87226c4f9d05e87fa221886ec42180
Saransh Gupta <saranshgupta1995@gmail.com>
2018-07-10 22:07:31 +0530

    change in dist path in app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'Desocialize/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + 'Desocialize/dist/index.html'));
 });
 
 const port = process.env.PORT || 3000;
