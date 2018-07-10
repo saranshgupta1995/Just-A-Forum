0da754758e0d985bc59b2f73bda2ef26519adf66
Saransh Gupta <saranshgupta1995@gmail.com>
2018-07-10 22:29:23 +0530

    hey bro

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/Desocialize')));
+app.use(express.static(path.join(__dirname, '/dist/DeSocialize')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'dist/Desocialize/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/DeSocialize/index.html'));
 });
 
 const port = process.env.PORT || 3000;
