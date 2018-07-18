c6bf15854e505cbb3ea7c4f01884d3e1e486d6be
Saransh Gupta <saranshgupta1995@gmail.com>
2018-07-18 21:38:19 +0530

    added environment vars

--- src/app/http/login-signup/httpUrls.ts
+++ src/app/http/login-signup/httpUrls.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HttpUrls {
 
-    addNewUserUrl ='http://obscure-sea-69570.herokuapp.com/addNewUser';
-    validateLoginUrl ='http://obscure-sea-69570.herokuapp.com/validateUserLogin'
+    addNewUserUrl =`${environment.serverUrl}/addNewUser`;
+    validateLoginUrl = `${environment.serverUrl}/validateUserLogin`;
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    serverUrl: "http://obscure-sea-69570.herokuapp.com"
 };
