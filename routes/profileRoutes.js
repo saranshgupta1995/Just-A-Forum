94391ad1db3c2bfa04354d808fad8d581b7790f1
Saransh Gupta <saranshgupta1995@gmail.com>
2018-08-01 21:13:39 +0530

    rewrite

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,13 +1,10 @@
 var router = require('express').Router();
 var profileDbOperations = require('./../database/profileDbOperations');
 
-router.post('/profile', (req, res) => {
-    let user = req.body.username
-    
-    profileDbOperations.dbOpr.findDoc('UserProfiles', { username: user }).then(oprRes => {
-        oprRes.forEach(x => {
-            res.send(x);
-        })
+router.post('/getprofile', (req, res) => {
+
+    profileDbOperations.findProfile(req.body.username).then(oprRes => {
+        res.send(oprRes);
     })
 
 });
