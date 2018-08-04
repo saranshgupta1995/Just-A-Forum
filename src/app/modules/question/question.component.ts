62a08c9c9228d11e230d95e3af7e791ec0370e95
Saransh Gupta <saranshgupta1995@gmail.com>
2018-08-04 12:43:59 +0530

    added question route anim

--- src/app/modules/question/question.component.ts
+++ src/app/modules/question/question.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../../http/question/question.service';
 import { ActivatedRoute } from '../../../../node_modules/@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
+    animations: [
+        trigger(
+            'enterAnimation', [
+                transition(':enter', [
+                    style({opacity: 0 }),
+                    animate('150ms', style({opacity: 1 }))
+                ]),
+            ]
+        )
+    ],
 })
 export class QuestionComponent implements OnInit {
 
