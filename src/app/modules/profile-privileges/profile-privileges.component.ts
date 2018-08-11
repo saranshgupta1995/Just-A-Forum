b3861a664effcbecb7a9ca3eec678a96462797e7
Saransh Gupta <saranshgupta1995@gmail.com>
2018-08-11 21:57:22 +0530

    fetching username in child

--- src/app/modules/profile-privileges/profile-privileges.component.ts
+++ src/app/modules/profile-privileges/profile-privileges.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
 
 @Component({
-  selector: 'profile-privileges',
-  templateUrl: './profile-privileges.component.html',
-  styleUrls: ['./profile-privileges.component.css']
+    selector: 'profile-privileges',
+    templateUrl: './profile-privileges.component.html',
+    styleUrls: ['./profile-privileges.component.css']
 })
 export class ProfilePrivilegesComponent implements OnInit {
+    username: string;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute) {
+        route.parent.params.subscribe(x =>
+             this.username=x['username']
+            );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
