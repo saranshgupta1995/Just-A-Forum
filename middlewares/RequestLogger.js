f2d586d3623f3bcfa0a1477b5b81db3b23154898
Saransh Gupta <saranshgupta1995@gmail.com>
2018-08-15 22:13:00 +0530

    added token validation middleware

--- middlewares/RequestLogger.js
+++ middlewares/RequestLogger.js
@@ -1,5 +1,4 @@
 module.exports = function (req, res, next) {
     req.requestTime = Date.now();
-    console.log(req.requestTime);
     next()
 }
---
+++ middlewares/Validation.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+
+    if (req.body["userName"] && req.body["password"]) {
+        jwt.sign({ user: req.body.userName, address: (req.connection.remoteAddress || req.socket.remoteAddress) }, 'secretkey', { expiresIn: "10h" }, (err, token) => {
+            req.token = token;
+            next();
+        })
+    } else {
+        let authHeader = req.headers['author'];
+        if (authHeader) {
+            jwt.verify(authHeader, 'secretkey', (err, authData) => {
+                if (err) {
+                    res.sendStatus(403);
+                } else {
+                    
+                    if (authData.address != (req.connection.remoteAddress || req.socket.remoteAddress)) {
+                        res.sendStatus(403);
+                    } else {
+                        if(req.body['token']=='my_uniq_token'){
+                            res.send(authData)
+                        }else{
+                            next();
+                        }
+                    }
+
+                }
+            })
+        } else {
+            res.sendStatus(403);
+        }
+    }
+}
