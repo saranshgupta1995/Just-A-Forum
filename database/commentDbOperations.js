0917da71b11c2a1c00037b20d0b3f9abf71bbab2
Saransh Gupta <saranshgupta1995@gmail.com>
2018-08-15 22:11:45 +0530

    added relevancy

--- database/commentDbOperations.js
+++ database/commentDbOperations.js
@@ -5,6 +5,7 @@ commentDbOperations.tableName = 'Comments';
 
 commentDbOperations.addComment = function (comment_data) {    
     comment_data.commentId = comment_data.quesId + '.' + (comment_data.commentId + 1);
+    comment_data.relevancy = 0;
     delete comment_data['quesId'];
     return commentDbOperations.dbOpr.insertOneOpr(commentDbOperations.tableName, comment_data);
 }
