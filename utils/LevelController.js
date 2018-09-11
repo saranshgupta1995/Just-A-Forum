956d6e41c0531f1f5b159cafaf47311a7a7f3468
Saransh Gupta <saranshgupta1995@gmail.com>
2018-09-11 22:36:31 +0530

    bugfix

--- utils/LevelController.js
+++ utils/LevelController.js
@@ -1,8 +1,10 @@
 let levelController = {};
-let profileDbOpr=require('./../database/profileDbOperations')
+let profileDbOpr = require('./../database/profileDbOperations')
 
+levelController.checkLevel0 = (data) => true;
 levelController.checkLevel1 = (data) => data.worth > 199;
 levelController.checkLevel2 = (data) => data.worth > 299;
+levelController.checkLevel3 = (data) => data.worth > 699;
 
 levelController.fetchData = (username) => {
     return profileDbOpr.findProfile(username).then(oprRes => {
@@ -11,34 +13,36 @@ levelController.fetchData = (username) => {
 }
 
 levelController.runData = (data) => {
-    let checkFuncs = ['checkLevel1', 'checkLevel2'];
+    let checkFuncs = ['checklevel0','checkLevel1', 'checkLevel2', 'checkLevel3'];
     let found = false;
     levelDict = {
         0: 'zero',
         1: 'one',
-        2: 'two'
+        2: 'two',
+        3: 'three'
     }
-    for (var func = 0; func < checkFuncs.length; func++) {
+    for (var func = 1; func <= checkFuncs.length; func++) {
         found = levelController[checkFuncs[func]](data);
+        console.log(func, found)
         if (!found)
             break
     }
-    return levelDict[Math.max(0,func-1)]
+    return levelDict[func - 1]
 }
 
-levelController.getLevel=(username, changeObj={
-    worth:0
-})=>{
+levelController.getLevel = (username, changeObj = {
+    worth: 0
+}) => {
     return levelController.fetchData(username)
-    .then(data=>{
-        let oldLevel=levelController.runData(data);
-        data.worth+=changeObj.worth;
-        let newLevel = levelController.runData(data);
-        return {
-            hasChange:oldLevel!==newLevel,
-            level:newLevel
-        }
-    })
+        .then(data => {
+            let oldLevel = levelController.runData(data);
+            data.worth += changeObj.worth;
+            let newLevel = levelController.runData(data);
+            return {
+                hasChange: oldLevel !== newLevel,
+                level: newLevel
+            }
+        })
 }
 
 module.exports = levelController;
